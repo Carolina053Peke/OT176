98a82da2be9cb44ff9b7c7ecb6291dddc805ec55
Dany Renaudier <dani11_21@hotmail.com>
2022-04-25 02:55:14 -0300

    Users soft delete route fixed

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,6 @@ const userValidation = require('../validations/user.js');
 
 router.post('/auth/signup', userValidation.signup, userController.signup);
 router.post('/auth/login', userValidation.login, userController.login);
-router.get('/auth/me', userValidation.authorizations.token, userController.getData);
 router.delete('/:id', [], userController.delete);
 
 
