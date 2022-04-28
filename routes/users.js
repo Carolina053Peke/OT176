5cfc62a734680cea5624b9398fe2cef65f066b69
tadeogavensky <tadeogavensky.email@gmail.com>
2022-04-28 14:35:53 -0300

    cambios en las rutas

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,11 @@ const awsImageUploader = require('../utils/awsImageUploader')
 
 
 router.get('/users', authAdmin, userController.userList);
-router.get('/auth/me', userValidation.authorizations.token, userController.getData);
+/* router.get('/auth/me', userValidation.authorizations.token, userController.getData); */
 router.post("/users/:id", userController.userEdit);
 router.post('/signup', userValidation.signup, userController.signup)
 router.post('/login', userValidation.login, userController.login)
-router.post('/auth/upload',authAdmin, imageValidation, upload, awsImageUploader)
-router.delete('/:id', [], userController.delete);
+router.post('/auth/awsImgUpload',authAdmin, upload, imageValidation, awsImageUploader)
+/* router.delete('/:id', [], userController.delete); */
 
 module.exports = router;
