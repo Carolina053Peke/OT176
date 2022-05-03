b0fa9f5dcb2a9beec440b4b70234813ff9943b2f
Markos <markosmk@me.com>
2022-05-03 12:07:41 -0300

    update: add create controller and route

--- controllers/slideController.js
+++ controllers/slideController.js
@@ -1,4 +1,5 @@
 const { Slides } = require('../models');
+const awsUpload = require('../utils/awsUpload');
 
 const getSlides = async (req, res, next) => {
   try {
@@ -9,6 +10,24 @@ const getSlides = async (req, res, next) => {
   }
 };
 
+const createSlide = async (req, res, next) => {
+  const { text, organizationId } = req.body;
+  let { order } = req.body;
+  try {
+    if (typeof req.file === 'undefined') throw new Error('Image is required');
+    if (!order) {
+      order = await Slides.max('order') + 1;
+    }
+    const imageUrl = await awsUpload(req.file);
+    const slide = await Slides.create({
+      imageUrl, text, order, organizationId,
+    });
+    res.status(201).json({ slide });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
-  getSlides,
+  getSlides, createSlide,
 };
--- routes/slides.js
+++ routes/slides.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { getSlides } = require('../controllers/slideController');
+const { getSlides, createSlide } = require('../controllers/slideController');
 const authenticated = require('../middlewares/authenticated');
 const authAdmin = require('../middlewares/authAdmin');
+const upload = require('../utils/multer');
 
 const router = express.Router();
 
 router.get('/', authenticated, authAdmin, getSlides);
+router.post('/', authenticated, authAdmin, upload('imageUrl'), createSlide);
 
 module.exports = router;
