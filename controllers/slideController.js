bbd1eeec6b37cc15f5287a65c1f7a2213be861a6
Markos <markosmk@me.com>
2022-05-03 17:08:19 -0300

    OT176-61 endpoint delete slides

--- controllers/slideController.js
+++ controllers/slideController.js
@@ -9,6 +9,19 @@ const getSlides = async (req, res, next) => {
   }
 };
 
+const deleteSlide = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) throw new Error('Invalid Id');
+    const slide = await Slides.findByPk(id);
+    if (!slide) throw new Error('Item not found');
+    await slide.destroy();
+    res.json({ message: 'Delete Success', slide });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
-  getSlides,
+  getSlides, deleteSlide,
 };
--- routes/slides.js
+++ routes/slides.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getSlides } = require('../controllers/slideController');
+const { getSlides, deleteSlide } = require('../controllers/slideController');
 const authenticated = require('../middlewares/authenticated');
 const authAdmin = require('../middlewares/authAdmin');
 
 const router = express.Router();
 
 router.get('/', authenticated, authAdmin, getSlides);
+router.delete('/:id', authenticated, authAdmin, deleteSlide);
 
 module.exports = router;
