26fb50ec86734bfb7f3b7a6970c9f5f8e28cf827
Ema Romero López <59179081+Emarlcoder@users.noreply.github.com>
2022-05-20 19:35:02 -0300

    Update categoryController.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,128 +1,24 @@
 const { validationResult } = require('express-validator');
 const db = require('../models');
-const { Category } = require('../models');
 
 const categoryController = {
   // Start Categories CRUD
-  categoryCreate: (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array(),
-      });
-    }
-    const { name, description, image } = req.body;
-    db.Category.create({
-      name,
-      description,
-      image,
+	@@ -26,21 +27,26 @@ const categoryController = {
+      })
+      .catch((error) => res.json(error));
+  },
+  categoryList: (req, res) => {
+    db.Category.findAll({
+      attributes: ['name'],
     })
       .then((result) => {
         const response = {
           status: 200,
-          message: 'Category created successfully!',
+          message: 'OK',
           data: result,
         };
         res.json(response);
       })
-      .catch((error) => res.json(error));
-  },
-  categoryList: async (req, res) => {
-    const { limit = 10, page = 0 } = req.query;
-    try {
-      const categories = await Category.findAndCountAll({
-        limit: +limit,
-        offset: limit * page,
+      .catch((error) => {
+        res.json(error);
       });
-      const { count: totalItems, rows: results } = categories;
-      const totalPages = Math.ceil(totalItems / limit);
-      res.json({
-        prevPage: page <= 0 ? '' : +page - 1,
-        nextPage: page >= totalPages ? '' : +page + 1,
-        currentPage: page ? +page : 0,
-        totalPages,
-        totalItems,
-        results,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
-  categoryEdit: (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(400).json({
-        errors: errors.array(),
-      });
-    }
-    const category = db.Category.findByPk(req.params.id);
-    if (category === null) {
-      const response = {
-        status: 404,
-        message: 'Category not found',
-        data: category,
-      };
-      res.json(response);
-    } else {
-      const { name, description, image } = req.body;
-      db.Category.update(
-        {
-          name,
-          description,
-          image,
-        },
-        {
-          where: {
-            id: req.params.id,
-          },
-        }
-      )
-        .then((result) => {
-          const response = {
-            status: 200,
-            message: 'Category updated successfully!',
-            data: result,
-          };
-          res.json(response);
-        })
-        .catch((error) => res.json(error));
-    }
-  },
-  categoryDetail: async (req, res) => {
-    try {
-      const category = await Category.findByPk(req.params.id);
-      res.status(200).json(category);
-    } catch (error) {
-      next(error);
-    }
-  },
-  categoryDelete: (req, res) => {
-    const category = db.Category.findByPk(req.params.id);
-    if (category === null) {
-      const resolve = {
-        status: 404,
-        message: 'Category not found',
-        data: category,
-      };
-      res.json(resolve);
-    } else {
-      db.Category.destroy({
-        where: {
-          id: req.params.id,
-        },
-      })
-        .then((result) => {
-          const resolve = {
-            status: 204,
-            message: 'Category deleted successfully!',
-            data: result,
-          };
-          res.json(resolve);
-        })
-        .catch((error) => res.json(error));
-    }
-  },
-  // End Categories CRUD
-};
-
-module.exports = categoryController;
