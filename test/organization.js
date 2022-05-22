3677271ca98300c033ac70d2c2b67541beae3877
Ema Romero López <emarlcoder@gmail.com>
2022-05-22 19:01:12 -0300

    arreglando pr

--- test/organization.js
+++ test/organization.js
@@ -7,34 +7,34 @@ const admin = { email: 'test@test.com', password: process.env.ADMIN_PASSWORD };
 const user = { email: 'user@test.com', name: 'Test user' };
 const unkonwnUser = { email: '', name: '' };
 
-let token
-let name
-let id
+let token;
+let name;
+let id;
 
 const organization = {
-  name: "Brubank",
-  image: "logo.png",
-  address: "Av Libertador 12453",
-  phone: "114346425346",
-  email: "brubank@gmail.com",
-  welcomeText: "Bienvenidos!",
-  aboutUsText: "Somos Brubank",
-  facebook: "facebook.com/brubank",
-  instagram: "instagram.com/brubank",
-  linkedin: "linkedin.com/brubank"
+  name: 'Brubank',
+  image: 'logo.png',
+  address: 'Av Libertador 12453',
+  phone: '114346425346',
+  email: 'brubank@gmail.com',
+  welcomeText: 'Bienvenidos!',
+  aboutUsText: 'Somos Brubank',
+  facebook: 'facebook.com/brubank',
+  instagram: 'instagram.com/brubank',
+  linkedin: 'linkedin.com/brubank',
 };
 const updatedOrganization = {
-  name: "Brubank2",
-  image: "logo.png",
-  address: "Av Libertador 12453",
-  phone: "114346425346",
-  email: "brubank@gmail.com",
-  welcomeText: "Bienvenidos!",
-  aboutUsText: "Somos Brubank",
-  facebook: "facebook.com/brubank",
-  instagram: "instagram.com/brubank",
-  linkedin: "linkedin.com/brubank"
-}
+  name: 'Brubank2',
+  image: 'logo.png',
+  address: 'Av Libertador 12453',
+  phone: '114346425346',
+  email: 'brubank@gmail.com',
+  welcomeText: 'Bienvenidos!',
+  aboutUsText: 'Somos Brubank',
+  facebook: 'facebook.com/brubank',
+  instagram: 'instagram.com/brubank',
+  linkedin: 'linkedin.com/brubank',
+};
 
 describe('ong', () => {
   describe('/get token', () => {
@@ -75,32 +75,33 @@ describe('ong', () => {
   describe('/get an ong', () => {
     it('should fetch the ong with the query name', (done) => {
       chai
-      .request(server)
-      .get(`/organization/public?name=${name}`)
-      .set({
-        Authorization: `Bearer ${token}`,
-      })
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('name');
-        res.body.should.have.property('image');
-        res.body.should.have.property('phone');
-        res.body.should.have.property('address');
-        res.body.should.have.property('facebook');
-        res.body.should.have.property('instagram');
-        res.body.should.have.property('linkedin');
-        res.body.should.have.property('slides');
-        res.body.name.should.be.a('string');
-        res.body.image.should.be.a('string');
-        res.body.phone.should.be.a('number');
-        res.body.address.should.be.a('string');
-        res.body.facebook.should.be.a('string');
-        res.body.instagram.should.be.a('string');
-        res.body.linkedin.should.be.a('string');
-        res.body.slides.should.be.a('array');
-        done();
-      });
+        .request(server)
+        .get(`/organization/public?name=${name}`)
+        .set({
+          Authorization: `Bearer ${token}`,
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('name');
+          res.body.should.have.property('image');
+          res.body.should.have.property('phone');
+          res.body.should.have.property('address');
+          res.body.should.have.property('facebook');
+          res.body.should.have.property('instagram');
+          res.body.should.have.property('linkedin');
+          res.body.should.have.property('slides');
+          res.body.name.should.be.a('string');
+          res.body.image.should.be.a('string');
+          res.body.phone.should.be.a('number');
+          res.body.address.should.be.a('string');
+          res.body.facebook.should.be.a('string');
+          res.body.instagram.should.be.a('string');
+          res.body.linkedin.should.be.a('string');
+          res.body.slides.should.be.a('array');
+          done();
+        });
     });
   });
-})
+});
+//
