fa008164d6a7ed20a030475446c72888d6d5988f
Ema Romero López <emarlcoder@gmail.com>
2022-05-22 19:05:14 -0300

    Arreglando el pr

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,7 +3,26 @@ const db = require('../models');
 
 const categoryController = {
   // Start Categories CRUD
-	@@ -26,21 +27,26 @@ const categoryController = {
+  categoryCreate: (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    const { name, description, image } = req.body;
+    db.Category.create({
+      name,
+      description,
+      image,
+    })
+      .then((result) => {
+        const response = {
+          status: 200,
+          message: 'Category created successfully!',
+          data: result,
+        };
+        res.json(response);
       })
       .catch((error) => res.json(error));
   },
@@ -22,3 +41,99 @@ const categoryController = {
       .catch((error) => {
         res.json(error);
       });
+  },
+  categoryEdit: (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    const category = db.Category.findByPk(req.params.id);
+    if (category === null) {
+      const response = {
+        status: 404,
+        message: 'Category not found',
+        data: category,
+      };
+      res.json(response);
+    } else {
+      const { name, description, image } = req.body;
+      db.Category.update(
+        {
+          name,
+          description,
+          image,
+        },
+        {
+          where: {
+            id: req.params.id,
+          },
+        }
+      )
+        .then((result) => {
+          const response = {
+            status: 200,
+            message: 'Category updated successfully!',
+            data: result,
+          };
+          res.json(response);
+        })
+        .catch((error) => res.json(error));
+    }
+  },
+  categoryDetail: (req, res) => {
+    db.Category.findByPk({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then((result) => {
+        if (result === null) {
+          const response = {
+            status: 404,
+            message: 'Category not found',
+            data: result,
+          };
+          res.json(response);
+        } else {
+          const response = {
+            status: 200,
+            message: 'OK',
+            data: result,
+          };
+          res.json(response);
+        }
+      })
+      .catch((error) => res.json(error));
+  },
+  categoryDelete: (req, res) => {
+    const category = db.findByPk(req.params.id);
+    if (category === null) {
+      const resolve = {
+        status: 404,
+        message: 'Category not found',
+        data: category,
+      };
+      res.json(resolve);
+    } else {
+      db.Category.destroy({
+        where: {
+          id: req.params.id,
+        },
+      })
+        .then((result) => {
+          const resolve = {
+            status: 204,
+            message: 'Category deleted successfully!',
+            data: result,
+          };
+          res.json(resolve);
+        })
+        .catch((error) => res.json(error));
+    }
+  },
+  // End Categories CRUD
+};
+
+module.exports = categoryController;
