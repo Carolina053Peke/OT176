e7ae5ce3fb3420c2d489c275face67084622551d
Fede Cione <fede_cione_96@hotmail.com>
2022-05-23 23:01:26 -0300

    Fixing and clean routes and controller of Categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,131 +4,111 @@ const { pagination } = require('../utils/paginate');
 
 const categoryController = {
   // Start Categories CRUD
-  categoryCreate: (req, res) => {
+  categoryCreate: async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({
         errors: errors.array(),
       });
     }
-    const { name, description, image } = req.body;
-    db.Category.create({
-      name,
-      description,
-      image,
-    })
-      .then((result) => {
-        const response = {
-          status: 200,
-          message: 'Category created successfully!',
-          data: result,
-        };
-        res.json(response);
-      })
-      .catch((error) => res.json(error));
-  },
-  categoryList: (req, res) => {
-    const { limit, page } = req.query;
-    db.Category.findAll({
-      attributes: ['name'],
-    })
-      .then(async (result) => {
-        const response = await pagination(db.Category, {}, page, limit);
-        res.json(response);
-      })
-      .catch((error) => {
-        res.json(error);
+    try {
+      const { name, description, image } = req.body;
+      const data = await db.Category.create({
+        name,
+        description,
+        image,
       });
+      res.status(200).json({
+        message: 'Category created successfully!',
+        data,
+      });
+    } catch (error) {
+      res.json(error);
+    }
   },
-  categoryEdit: (req, res) => {
+  categoryList: async (req, res) => {
+    try {
+      const { limit, page } = req.query;
+      const response = await pagination(db.Category, {}, page, limit);
+      res.json(response);
+    } catch (error) {
+      res.json(error);
+    }
+  },
+  categoryEdit: async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({
         errors: errors.array(),
       });
     }
-    const category = db.Category.findByPk(req.params.id);
-    if (category === null) {
-      const response = {
-        status: 404,
-        message: 'Category not found',
-        data: category,
-      };
-      res.json(response);
-    } else {
-      const { name, description, image } = req.body;
-      db.Category.update(
-        {
-          name,
-          description,
-          image,
-        },
-        {
+    try {
+      const category = db.Category.findByPk(req.params.id);
+      if (category === null) {
+        const response = {
+          status: 404,
+          message: 'Category not found',
+          data: category,
+        };
+        res.json(response);
+      } else {
+        const { name, description, image } = req.body;
+        await db.Category.update(
+          {
+            name,
+            description,
+            image,
+          },
+          {
+            where: {
+              id: req.params.id,
+            },
+          },
+        );
+        res.status(200).json({
+          message: 'Category updated successfully!',
+        });
+      }
+    } catch (error) {
+      res.json(error);
+    }
+  },
+  categoryDetail: async (req, res) => {
+    try {
+      const categoryFiltered = await db.Category.findByPk(req.params.id);
+      if (categoryFiltered === null) {
+        res.status(404).json({
+          message: 'Category not found',
+          data: categoryFiltered,
+        });
+      }
+      res.status(200).json({
+        message: 'OK',
+        data: categoryFiltered,
+      });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  categoryDelete: async (req, res) => {
+    try {
+      const category = await db.Category.findByPk(req.params.id);
+      if (category === null) {
+        res.status(404).json({
+          message: 'Category not found',
+        });
+      } else {
+        await db.Category.destroy({
           where: {
             id: req.params.id,
           },
-        }
-      )
-        .then((result) => {
-          const response = {
-            status: 200,
-            message: 'Category updated successfully!',
-            data: result,
-          };
-          res.json(response);
-        })
-        .catch((error) => res.json(error));
-    }
-  },
-  categoryDetail: (req, res) => {
-    db.Category.findByPk({
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then((result) => {
-        if (result === null) {
-          const response = {
-            status: 404,
-            message: 'Category not found',
-            data: result,
-          };
-          res.json(response);
-        } else {
-          const response = {
-            status: 200,
-            message: 'OK',
-            data: result,
-          };
-          res.json(response);
-        }
-      })
-      .catch((error) => res.json(error));
-  },
-  categoryDelete: (req, res) => {
-    const category = db.findByPk(req.params.id);
-    if (category === null) {
-      const resolve = {
-        status: 404,
-        message: 'Category not found',
-        data: category,
-      };
-      res.json(resolve);
-    } else {
-      db.Category.destroy({
-        where: {
-          id: req.params.id,
-        },
-      })
-        .then((result) => {
-          const resolve = {
-            status: 204,
-            message: 'Category deleted successfully!',
-            data: result,
-          };
-          res.json(resolve);
-        })
-        .catch((error) => res.json(error));
+        });
+        res.status(200).json({
+          message: 'Category deleted successfully!',
+        });
+      }
+    } catch (error) {
+      res.json(error);
     }
   },
   // End Categories CRUD
